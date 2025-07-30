3bce5381f99cf25576205aec25e637ac185fa76c
muhammadzeeshanhassan <muhammad.zeeshan@devsinc.com>
2025-07-30 17:16:21 +0500

    devise for user added

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  devise_for :users, controllers: {
+    registrations: 'user/registrations',
+  }
 end
