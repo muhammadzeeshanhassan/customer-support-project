6594dfef079db3bda2d577cebff609560dc8d077
muhammadzeeshanhassan <muhammad.zeeshan@devsinc.com>
2025-08-04 16:04:05 +0500

    json rendering

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -40,7 +40,7 @@ class TicketController < ApplicationController
   end
 
   def show
-    # render json: @ticket, status: :ok
+    render json: @ticket, status: :ok
     # respond_to do |format|
     # format.html  # renders app/views/ticket/show.html.erb → your React Mount
     # format.json { render json: @ticket }
