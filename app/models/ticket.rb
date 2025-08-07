a9f041a923d9d1060e83a6627e9249ad7bf2340c
muhammadzeeshanhassan <muhammad.zeeshan@devsinc.com>
2025-08-07 14:35:53 +0500

    please move validations and associations to top above the methods in models - resolved

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,12 @@
 class Ticket < ApplicationRecord
-  enum :status, { open: 0, pending: 1, closed: 2 }
-  enum :priority, { low: 0, medium: 1, high: 2 }
+  enum status:   { open: 0, pending: 1, closed: 2 }
+  enum priority: { low: 0, medium: 1, high: 2 }
+
+  belongs_to :customer, class_name: "User"
+  belongs_to :agent, class_name: "User", optional: true
+
+  validates :subject, presence: true, length: { minimum: 15, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 300 }
 
   after_update :notify_customer_of_status_change
 
@@ -9,10 +15,4 @@ class Ticket < ApplicationRecord
   def notify_customer_of_status_change
     TicketMailer.notify_customer_of_status_change(self).deliver_later
   end
-
-  belongs_to :customer, class_name: "User"
-  belongs_to :agent, class_name: "User", optional: true
-
-  validates :subject, presence: true
-  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  enum :role, { admin: 0, agent: 1, customer: 2 }
+  enum role: { admin: 0, agent: 1, customer: 2 }
 
   has_many :tickets, foreign_key: :customer_id, dependent: :destroy
-  has_many :assigned_tickets, class_name: "Ticket", foreign_key: :agent_id
+  has_many :assigned_tickets,  class_name: "Ticket", foreign_key: :agent_id
 end
